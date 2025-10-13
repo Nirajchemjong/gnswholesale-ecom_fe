8ef6cdfd166702711091a586837c851014d2d7f6
nirajchemjong <nirajlimbu7@gmail.com>
2025-10-13 21:55:54 +1100

    Enhance PremiumSelectionCard with improved image scaling effect and updated button styles; adjust layout for better spacing

--- ui/cards/PremiumSelectionCard.tsx
+++ ui/cards/PremiumSelectionCard.tsx
@@ -2,17 +2,17 @@ const PremiumSelectionCard = ({ image, content, isBulkPricing }: { image: string
   return (
     <div className="relative w-full rounded-lg overflow-hidden border border-gray-200 p-4 shadow-md">
         
-        <div className="relative aspect-[4/3] w-full">
-            <img src={image} alt="Premium Selection" className="absolute inset-0 w-full h-full object-cover rounded-lg"/>
+        <div className="relative aspect-[4/3] w-full overflow-hidden">
+            <img src={image} alt="Premium Selection" className="absolute inset-0 w-full h-full object-cover rounded-lg hover:scale-160 transition-all duration-100 ease-in-out transform-gpu origin-center-right"/>
             {isBulkPricing && (
                 <div className="absolute bottom-2 left-2">
                     <h3 className="text-medium leading-normal bg-[#FFB200] rounded-lg p-2">Bulk Pricing Available</h3>
                 </div>
             )}
         </div>
-    <div className="content-container flex flex-col gap-2">
+    <div className="content-container flex flex-col gap-2 mt-2">
         <h5 className="text-medium font-light line-clamp-2">{content}</h5>
-     <button className="text-sm border-2 border-gray-200 rounded-full p-2">View Details</button>
+     <button className="text-sm border-2 border-gray-200 rounded-full p-2 hover:bg-primary-2 hover:text-white transition-all duration-200 ease-in-out">View Details</button>
     </div>
     </div>
   )
