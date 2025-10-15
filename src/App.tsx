36b6d75920aec6d3a7964d8440bfea02ecaccc7a
nirajchemjong <nirajlimbu7@gmail.com>
2025-10-15 18:34:51 +1100

    Add routing functionality with TanStack Router; create Products page and integrate into App component; update package dependencies for React and Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import "./App.css"
-import Home from "./pages/home";
+import { createRootRoute, createRoute, createRouter, RouterProvider } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import "./App.css";
+import Home from './pages/home';
+import ProductsIndex from './pages/Products/Index';
+const rootRoute = createRootRoute()
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: Home,
+})
+
+const productsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/products',
+  component: ProductsIndex,
+})
+
+const routeTree = rootRoute.addChildren([indexRoute, productsRoute])
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
 function App() {
   return (
-    <Home />
-  );
+    <>
+      <RouterProvider router={router} />
+      <TanStackRouterDevtools router={router} />
+    </> 
+  )
 }
 
 export default App;
---
+++ src/pages/Products/Index.tsx
@@ -0,0 +1,8 @@
+
+const Index = () => {
+  return (
+    <div>Product Details page</div>
+  )
+}
+
+export default Index
